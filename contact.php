ed72c016081e6d24350119e9161c7580d1877aa4
skoth001 <skoth001@ucr.edu>
2014-09-11 13:42:28 -0700

    added contact form to contact.php

--- contact.php
+++ contact.php
@@ -7,8 +7,24 @@
       <section id="primary">
         <h3>General Information</h3>
         <p>We are ready to start delivering amazing indian food right now so give us a call at your earliest convenience.</p>
-        <p>Email and phone are probably the best way to reach us, and twitter is the best place to leave comments to help our business.</p>
+        <p>You can always leave comments below.</p>
+
+        <form class ="contact-form" method="post">
+          <label for ="name">Name </label>
+          <input type = "text" name="name" id="name"><br>
+                
+          <label for ="email">Email</label>    
+          <input type = "email" name="email" id="email"><br>                
+                  
+          <label for ="message">Message</label>
+          <textarea name="message" id="message"></textarea><br>
+                
+          <input type="submit" value="Send">
+        </form>         
+        
       </section>
+
+
       <section id="secondary">
         <h3>Contact Details</h3>
         <ul class="contact-info">
@@ -17,4 +33,7 @@
           <li class="twitter"><a href="http://twitter.com/intent/tweet?screen_name=shakotha">@shakotha</a></li>
         </ul>
       </section>
+      
+
+
 <?php include('includes/footer.php'); ?>
